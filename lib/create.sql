5480331a5e1f73111401a9d0333fa0e4db278bad
tillenotuoma <totuoma27@gmail.com>
2025-03-03 23:35:44 +0300

    done tests

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE bears (
     id INTEGER PRIMARY KEY,
     name TEXT,
-    age INTEGER
-    sex TEXT
-    color TEXT
-    temperament TEXT
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
     alive BOOLEAN 
-    );
+);
